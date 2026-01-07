0cdbe1212e6d098fa9e462a9d727d5b724c397f4
Daniel Junior <danieljunior1903@gmail.com>
2026-01-07 11:13:34 -0400

    Java App - learning and practicing with Junit - first commit

---
+++ src/com/sandaniel/appsdeveloperblog/Calculator.java
@@ -0,0 +1,8 @@
+package com.sandaniel.appsdeveloperblog;
+
+public class Calculator {
+	
+	public int integerDivision(int dividend, int divisor) {
+		return dividend/divisor;
+	}
+}
---
+++ test/com/sandaniel/appsdeveloperblog/CalculatorTest.java
@@ -0,0 +1,15 @@
+package com.sandaniel.appsdeveloperblog;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+		System.out.println("Not implemented yet");
+	}
+
+}
