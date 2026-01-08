be2cce75cb350f361532157a82e5ad0427d7bd25
Daniel Junior <danieljunior1903@gmail.com>
2026-01-08 10:04:03 -0400

    Java app/Junit - Assertion message

--- test/com/sandaniel/appsdeveloperblog/CalculatorTest.java
+++ test/com/sandaniel/appsdeveloperblog/CalculatorTest.java
@@ -1,7 +1,7 @@
 package com.sandaniel.appsdeveloperblog;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 import org.junit.jupiter.api.Test;
 
@@ -18,7 +18,7 @@ class CalculatorTest {
 		int result = calculator.integerDivision(4, 2);
 		
 		//Assert
-		assertEquals(2, result);
+		assertEquals(2, result-1," The result of 4/2 must be 2, and it is not!");
 	}
 
 }
