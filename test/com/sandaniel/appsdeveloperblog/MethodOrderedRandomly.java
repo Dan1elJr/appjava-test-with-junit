2cbb04242a46a4980bbf75df3f5d1ec60a31513b
Daniel Junior <danieljunior1903@gmail.com>
2026-01-27 10:12:25 -0400

    Java/JUnit - @TestMethodOrder annotation

---
+++ test/com/sandaniel/appsdeveloperblog/MethodOrderedRandomly.java
@@ -0,0 +1,26 @@
+package com.sandaniel.appsdeveloperblog;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.Random.class)
+public class MethodOrderedRandomly {
+	
+	@Test
+	void testA() {
+		System.out.println("Running test A");
+	}
+	
+	@Test
+	void testB() {
+		System.out.println("Running test B");
+	}
+	
+	@Test
+	void testC() {
+		System.out.println("Running test C");
+	}
+	
+	
+}
